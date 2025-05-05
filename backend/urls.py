06d3335cf318819e3b3d5b7fa57062c82e6c1687
KadirovKmck <kadirovvmuhammadaziz79.com>
2025-05-05 11:27:23 +0600

    desfrd

--- backend/urls.py
+++ backend/urls.py
@@ -12,12 +12,12 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("api.urls")),
+    path("api/", include("api.urls")),
 
     # schema & docs
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "docs",
+        "",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
