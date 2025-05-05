67996a08f16f8b4cab63acfbab35d1d199553e6c
KadirovKmck <kadirovvmuhammadaziz79.com>
2025-05-05 11:05:59 +0600

    asdf

--- backend/settings.py
+++ backend/settings.py
@@ -27,9 +27,10 @@ if DATABASE_URL:
         )
     }
 else:
+    # fallback только для локального запуска
     DATABASES = {
         "default": {
-            "ENGINE": 'django.db.backends.sqlite3',
+            "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3"
         }
     }
