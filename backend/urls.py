58e5493d5ddf5254320d578e7bf8b2adae53385f
KadirovKmck <kadirovvmuhammadaziz79.com>
2025-05-05 10:07:36 +0600

    sss

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     # schema & docs
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "",
+        "docs",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
