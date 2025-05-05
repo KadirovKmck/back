dff5285ec0ab8b47938f09cbffa9eae5ca07dd51
KadirovKmck <kadirovvmuhammadaziz79.com>
2025-05-05 09:45:58 +0600

    sdf

--- backend/settings.py
+++ backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    "drf_spectacular", 
+    "drf_spectacular",
     'api',
     'media',
     "rest_framework",
